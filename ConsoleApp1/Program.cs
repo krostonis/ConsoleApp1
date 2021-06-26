cf37cf147072cb844a8f1ad6e4b14914e7a6521f
Maksim Bobrov <krostonis@gmail.com>
2021-06-26 16:24:21 +0300

    Update massege

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Maks!");
+            Console.WriteLine("Nice Day");
         }
     }
 }
